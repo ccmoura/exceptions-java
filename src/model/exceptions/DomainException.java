1d322455954bae12a255072152be8b9a5371b63c
Christopher Moura <ccmoura@inf.ufpel.edu.br>
2019-02-27 03:47:42 -0300

    Good solution

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+/**
+ *
+ * @author chris
+ */
+public class DomainException extends Exception {
+    
+    public DomainException(String msg){
+        super(msg);
+    }
+}
